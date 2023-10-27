5fd2649a595cf90a1d03b083676c072a7659a827
enimad <aminedjellab92@gmail.com>
2023-10-28 01:05:49 +0200

    Homepage updated
    Everything is good and working correctly for the mobile version

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Fonction pour ouvrir le menu de nav mobile
+const navMobile = document.querySelector(".nav-mobile");
+nav.addEventListener("click", () => {
+    nav.classList.toggle("is-active");
+    navMobile.classList.toggle("active");
+})
+
+// Fonction qui assure la fermeture du menu de nav mobile en cas de clic sur les liens
+const links = document.querySelectorAll(".nav-mobile li");
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("is-active");
+        navMobile.classList.remove("active");
+    })
+})
