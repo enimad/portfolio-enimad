c6ec735b7b4e96abd97b3f4052e7daed5bbc88a4
enimad <aminedjellab92@gmail.com>
2023-10-31 22:16:50 +0100

    Project img container updated Created a function that allows it to move on click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+//**************************** SECTION HEADER ****************************//
 // Fonction pour ouvrir le menu de nav mobile
 const navMobile = document.querySelector(".nav-mobile");
+
 nav.addEventListener("click", () => {
     nav.classList.toggle("is-active");
     navMobile.classList.toggle("active");
@@ -7,6 +9,7 @@ nav.addEventListener("click", () => {
 
 // Fonction qui assure la fermeture du menu de nav mobile en cas de clic sur les liens
 const links = document.querySelectorAll(".nav-mobile li");
+
 links.forEach((link) => {
     link.addEventListener("click", () => {
         nav.classList.remove("is-active");
@@ -16,7 +19,20 @@ links.forEach((link) => {
 
 // Fonction qui assure la fermeture du menu de nav mobile en cas de clic sur le logo du site
 const logo = document.querySelector(".logo");
+
 logo.addEventListener("click", () => {
     nav.classList.remove("is-active");
     navMobile.classList.remove("active");
 })
+
+//**************************** SECTION PROJECTS ****************************// 
+// Fonction pour faire défiler l'image du projet
+let imgProjectContainer = document.querySelectorAll(".project-img-container");
+const imgProject = document.querySelectorAll(".project-img-container img");
+
+for (let i = 0; i < imgProjectContainer.length; i++) {
+    imgProjectContainer[i].addEventListener("click", () => {
+        imgProject[i].classList.toggle("active");
+    })
+}
+
