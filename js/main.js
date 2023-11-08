06b55a83d2f88685d4bf170cfa942e54ae7dcc09
enimad <aminedjellab92@gmail.com>
2023-11-08 17:33:49 +0100

    Updates js updated - fetching projects data from projects.json json updated index.html updated - static projects deleted

--- js/main.js
+++ js/main.js
@@ -26,38 +26,70 @@ logo.addEventListener("click", () => {
 })
 
 //**************************** SECTION PROJECTS ****************************// 
-// Fonction pour faire défiler l'image du projet
-let imgProjectContainer = document.querySelectorAll(".project-img-container");
-const imgProject = document.querySelectorAll(".project-img-container img");
-
-for (let i = 0; i < imgProjectContainer.length; i++) {
-    imgProjectContainer[i].addEventListener("click", () => {
-        imgProject[i].classList.toggle("active");
-    })
-}
+// Ajout dynamique des projets
 
 fetch("../assets/data/projects.json")
     .then(res => res.json())
     .then(data => {
-        // const ulElement = document.querySelector("ul");
+        data.forEach((projectData) => {
+            const projectContainer = document.createElement('div');
+            projectContainer.className = 'project-container';
 
-        // const liElement = document.createElement("li");
+            const projectImgContainer = document.createElement('div');
+            projectImgContainer.className = 'project-img-container';
 
-        // const aElement = document.createElement("a");
-        // aElement.href = "#";
-        // aElement.textContent = e;
+            const projectImage = document.createElement('img');
+            projectImage.src = projectData.image.src;
+            projectImage.alt = projectData.image.alt;
 
-        // liElement.appendChild(aElement);
+            projectImgContainer.appendChild(projectImage);
 
-        // ulElement.appendChild(liElement);
+            const projectTitle = document.createElement('h3');
+            projectTitle.textContent = projectData.title;
 
-        // const links = document.querySelectorAll("nav li");
-        // links.forEach((link) => {
-        //     link.addEventListener("click", () => {
-        //         nav.classList.remove("active");
-        //     })
-        // })
-        console.log(data)
+            const projectDescription = document.createElement('p');
+            projectDescription.innerHTML = projectData.content;
+
+            const projectLinks = document.createElement('div');
+            projectLinks.className = 'project-links';
+
+            const githubLink = document.createElement('a');
+            githubLink.href = projectData.codeHref;
+            githubLink.textContent = 'Code';
+            const githubIcon = document.createElement('img');
+            githubIcon.src = './assets/images/icons/icon-github.png';
+            githubIcon.alt = 'Icône GitHub';
+            githubLink.appendChild(githubIcon);
+
+            const liveLink = document.createElement('a');
+            liveLink.href = projectData.liveHref;
+            liveLink.textContent = 'Live';
+            const liveIcon = document.createElement('img');
+            liveIcon.src = './assets/images/icons/icon-external-link.png';
+            liveIcon.alt = 'Icône de lien externe';
+            liveLink.appendChild(liveIcon);
+
+            projectLinks.appendChild(githubLink);
+            projectLinks.appendChild(liveLink);
+
+            projectContainer.appendChild(projectImgContainer);
+            projectContainer.appendChild(projectTitle);
+            projectContainer.appendChild(projectDescription);
+            projectContainer.appendChild(projectLinks);
+
+            projects.appendChild(projectContainer);
+        });
+
+        // Fonction pour faire défiler l'image du projet
+        let imgProjectContainer = document.querySelectorAll(".project-img-container");
+        const imgProject = document.querySelectorAll(".project-img-container img");
+
+
+        for (let i = 0; i < imgProjectContainer.length; i++) {
+            imgProjectContainer[i].addEventListener("click", () => {
+                imgProject[i].classList.toggle("active");
+            })
+        }
     })
     .catch(e => {
         console.log(e);
